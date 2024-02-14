4a1dce37b0a71b0fbd8d4d9efcbcee7ed6c6126a
Iisakki Marjomäki <cycklingben@gmail.com>
2024-02-14 14:37:11 +0200

    teht 3.2 tehty

--- index.js
+++ index.js
@@ -19,12 +19,16 @@ let persons = [
     }
 ]
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+    res.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${Date()}</p>`)
 })
 
 const PORT = 3001
