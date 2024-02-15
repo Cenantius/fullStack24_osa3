7973c39555b9159c253ba8243915a465863bddd8
Iisakki Marjomäki <cycklingben@gmail.com>
2024-02-15 13:07:19 +0200

    teht 3.7 tehty

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
     {
@@ -87,4 +90,8 @@ app.get('/info', (req, res) => {
 const PORT = 3001
 app.listen(PORT, () =>{
     console.log(`Server running on port ${PORT}`)
+})
+
+morgan.token('host', function(req, res) {
+    return req.hostname
 })
