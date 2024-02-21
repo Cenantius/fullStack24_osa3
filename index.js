30e6a1808dc92913a31b530d5ab7bb47f9233fae
Iisakki Marjomäki <cycklingben@gmail.com>
2024-02-21 12:45:34 +0200

    commitointi ennen Render -deployta

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('body', req => {
     return JSON.stringify(req.body)
@@ -92,7 +94,7 @@ app.get('/info', (req, res) => {
     res.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${Date()}</p>`)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () =>{
     console.log(`Server running on port ${PORT}`)
 })
