52ee73361eeb9c0de13bef247b862e2eb143154d
Iisakki Marjomäki <cycklingben@gmail.com>
2024-03-13 14:56:12 +0200

    teht 3.22 tehty

--- models/person.js
+++ models/person.js
@@ -6,39 +6,39 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const numValidator = (value) => {
-    const regex = /^[0-9]{2,3}-[0-9]{7,}$\d{}/
-    return regex.test(value)
+  const regex = /^[0-9]{2,3}-[0-9]{7,}$\d{}/
+  return regex.test(value)
 }
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        validate: {
-            validator: numValidator,
-            message: props => `${props.value} is not in the correct format. Please try again!`
-        }
-    },
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: numValidator,
+      message: props => `${props.value} is not in the correct format. Please try again!`
+    }
+  },
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
